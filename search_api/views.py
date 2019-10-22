f75dea463994033f28fc509e2edafa6111a3b735
phidato <rizahmed.in@gmail.com>
2019-10-23 01:59:49 +0530

    update requirement file

--- search_api/views.py
+++ search_api/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 def search(request):
-	return HttpResponse("Implement here")
+	word = request.GET.get('word')
+	import pandas as pd
+
+	path_to_file = os.path.join(BASE_DIR, 'dataset')
+
+	user_info = pd.read_csv(path_to_file+'/word_search.tsv',delimiter='\t',encoding='utf-8')
+	print(list(user_info.columns.values)) #file header
+	print(user_info.head(35)) #last N rows
+
+	return HttpResponse("Implement here :: "+word)
